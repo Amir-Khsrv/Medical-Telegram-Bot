8f065ccd15679d09331c1fe0c2d214feb0374eb6
Amir-Khsrv <145700200+Amir-Khsrv@users.noreply.github.com>
2024-12-05 21:51:11 +0330

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from Main import app  # Import the Flask app from your main script
+# run.py
+import uvicorn
+from main import app  # Import the FastAPI app from the main file
 
-# Start FastAPI app
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
