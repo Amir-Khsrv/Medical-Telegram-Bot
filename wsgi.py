ac0f5ad8530f0d4c533aee23c95d651e1bae8621
Amir-Khsrv <145700200+Amir-Khsrv@users.noreply.github.com>
2024-12-05 20:41:36 +0330

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from Main import app  # Import the Flask app from your main script
 
 if __name__ == "__main__":
-    app_asgi.run(host="0.0.0.0", port=5000)
+    Server(app).run(host="0.0.0.0", port=5000)
