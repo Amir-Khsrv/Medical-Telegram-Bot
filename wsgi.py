5f51f594f92bf8750284f348ff2594969820e905
Amir-Khsrv <145700200+Amir-Khsrv@users.noreply.github.com>
2024-12-05 20:49:38 +0330

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from Main import app  # Import the Flask app from your main script
 
 if __name__ == "__main__":
-    Server(app).run(host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
