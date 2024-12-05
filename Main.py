73850d784a097c4c3f7b91d25661217c25e50eab
Amir-Khsrv <145700200+Amir-Khsrv@users.noreply.github.com>
2024-12-05 20:04:57 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -105,7 +105,7 @@ def webhook():
         print("Incoming update:", data)  # Log incoming data for debugging
         if data:
             update = Update.de_json(data, application.bot)  # Deserialize data
-            application.process_update(update)  # Process the update
+            application.asyncio.run(application.process_update(update))  # Fixing the warning
         return "OK", 200  # Return a successful HTTP status code
     except Exception as e:
         print("Error in webhook:", e)  # Log the error
